df573d7c88661404503efb093c097c0b64737048
Vishal sepaia <vishal.thakur607@gmail.com>
2021-04-30 23:42:15 +0530

    Getting started with Spring Security

---
+++ src/main/java/com/jordan/pma/security/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.jordan.pma.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("vishal")
+				.password("123")
+				.roles("USER")
+			.and()
+				.withUser("jordan")
+				.password("123")
+				.roles("USER")
+			.and()
+				.withUser("admin")
+				.password("123")
+				.roles("ADMIN");
+			
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/projects/new").hasRole("ADMIN")
+			.antMatchers("/employee/new").hasRole("ADMIN")
+			.antMatchers("/").authenticated().and().formLogin();
+			
+	}
+	
+	
+}
